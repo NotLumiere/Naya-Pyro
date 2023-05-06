8bc0bb763f78742a84fe0a860922e336be461a96
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-06 16:54:44 +0700

    Delete Ubot/core directory

--- Ubot/core/data.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton, WebAppInfo
-
-class Data:
-
-    text_help_menu = (
-        f"**Help Menu**\n** • Prefixes** : `None`"
-    )
-    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
